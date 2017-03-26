1144b686955d1ce6b5ef9db38bd53d616baf146c
PoC-Yeh <allisonxp7@gmail.com>
2017-03-26 20:38:40 +0800

    Update url_crawl.py

--- url_crawl.py
+++ url_crawl.py
@@ -9,9 +9,6 @@ domain = "https://www.ptt.cc/"
 url = "https://www.ptt.cc/bbs/MakeUp/index3.html"
 
 
-url_list = []
-
-
 def serp_title_link():
     title = soup.find_all("div", class_ = "title")
     for i in title:
@@ -28,49 +25,3 @@ def previous_page_link():
     previous = soup.find_all("a", class_ = "btn wide")[1]
     previous_url = domain + previous.get("href")
     return(previous_url)
-
-
-count = 1
-sleep = 0.1
-page_count = 0
-bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength) 
-
-while True:
-    serp_url = requests.get(url).text
-    soup = BeautifulSoup(serp_url, "html.parser")
-    serp_title_link()
-    page_count += 1 #bar
-    bar.update(page_count)  #bar
-    
-    #break when the last page is finished
-    if url == "https://www.ptt.cc//bbs/MakeUp/index1.html":
-        break
-    previous_page_link()
-    url = previous_page_link()
-    #print(url)
-  
-    #time.sleep(sleep)
-    #sleep += 0.01
-    
-    #write csv automatically after crawling every 500 pages
-    if count < 500:
-        count += 1
-    elif count == 500:
-        count = 1
-        for_csv_url_list = []
-        for i in url_list:
-            list_list = []
-            list_list.append(i)
-            for_csv_url_list.append(list_list)
-
-        f = open('ptt_makeup.csv', 'w')
-        w = csv.writer(f)
-        w.writerows(for_csv_url_list)
-
-        f.close()
-    
-    
-print(url_list[-1])
-
-#for i in url_list:
-#    print(i)
