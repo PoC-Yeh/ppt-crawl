52b8f19491d5ff8ab369e161e882368419815151
PoC-Yeh <allisonxp7@gmail.com>
2017-03-31 02:08:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import progressbar
 import re
 import requests
 import time
+import pickle
 from bs4 import BeautifulSoup
 from url_crawl import serp_title_link,previous_page_link
 from text_crawl import text_without_garbage
@@ -48,45 +49,54 @@ while True:
         f.close()
     
 
-    
+
 
 url_text_dict = {} 
 with_keyword_text = {}
 
 
 keyword = "資生堂"
-save_count = 1
+save_count = 0
 url_count = 0
 bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
 
 
 for url in url_list:
-    page_content = text_without_garbage(url)
-    #print(page_content)
-    url_text_dict[url] = page_content
-    
-    for sentence in page_content:
-        if keyword in sentence:
-            with_keyword_text[url] = page_content[3:]  #without meta data
+    try:
+        page_content = text_without_garbage(url)
+        #print(page_content)
+        url_text_dict[url] = page_content
+
+        for sentence in page_content:
+            if keyword in sentence:
+                with_keyword_text[url] = page_content[3:]  #without meta data
+                
+        url_count += 1 #bar
+        bar.update(url_count)  #bar
+
+        if save_count < 500:
+            save_count += 1
+
+        elif save_count == 500:
+            save_count = 1
+
+            f = open('makeup_text_new.csv', 'w')
+            w = csv.writer(f)
+            w.writerows(url_text_dict.items())
+            f.close()
+
+            f2 = open('shiseido_text_new.csv', 'w')
+            w2 = csv.writer(f2)
+            w2.writerows(with_keyword_text.items())
+            f2.close()
             
-    url_count += 1 #bar
-    bar.update(url_count)  #bar
+        if url_count % 2000 == 0:
+            time.sleep(60*15)
     
-    if save_count < 500:
-        save_count += 1
+    except AttributeError:
+        continue
         
-    elif save_count == 500:
-        save_count = 1
-        
-        f = open('makeup_text.csv', 'w')
-        w = csv.writer(f)
-        w.writerows(url_text_dict.items())
-        f.close()
-        
-        f2 = open('shiseido_text.csv', 'w')
-        w2 = csv.writer(f2)
-        w2.writerows(with_keyword_text.items())
-        f2.close()
-        
-    if url_count % 2000 == 0:
-        time.sleep(60*15)
+
+output1 = pickle.dump(new_url_text_dict, open("url_text_dict.txt", "wb"))
+ouptput2 = pickle.dump(new_with_keyword_text, open("with_keyword_text.txt", "wb"))
+
